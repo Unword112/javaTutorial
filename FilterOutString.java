dc1557ba250b6a305f1c1c39d5d087135e7658a3
Unword112 <pog123@hotmail.co.uk>
2021-05-04 11:26:06 +0700

    change device

---
+++ FilterOutString.java
@@ -0,0 +1,10 @@
+public class FilterOutString {
+    public static String[] filterArray(String[] str) {
+		String[] str = integers.spilt(", ");
+	}
+    public static void main(String[] args) {
+        FilterOutString f = new FilterOutString();
+        String[] char1 = {"1", "2", "a", "b"};
+        System.out.print(f.filterArray(char1));
+    }
+}
